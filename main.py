972701c6c6d154870d1fad1ae721a1b40ca69316
Scribl_ <walzspizzas@gmail.com>
2024-10-19 20:47:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ while cap.isOpened():
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 900:
+            continue
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame1, "Status: {}".format('Movement'), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 0, 255), 3)
+
+    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
     cv2.imshow("feed", frame1)
     frame1 = frame2
@@ -23,5 +32,5 @@ while cap.isOpened():
     if cv2.waitKey(40) == 27:
         break
 
-    cv2.destroyAllWindows()
-    cap.release()
+cv2.destroyAllWindows()
+cap.release()
